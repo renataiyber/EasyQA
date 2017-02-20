9244f66c4afe4c0aa267150e53c18ba9f3c7a792
RenaIyber <renata.iyber@thinkmobiles.com>
2017-02-20 16:50:56 +0200

    CreateNegative

--- src/Tests/CreateNewTestRun.java
+++ src/Tests/CreateNewTestRun.java
@@ -3,6 +3,7 @@ import Methods.CreateTestRun;
 import Methods.LogInPage;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
@@ -45,9 +46,22 @@ public class CreateNewTestRun extends BaseTest {
         CreateTestRun createTestRun = new CreateTestRun();
         createTestRun.newtestrun(driver);
         Thread.sleep(4000);
-        driver.findElement(By.xpath("//input[@name='commit']")).click();
-        Thread.sleep(4000);
+        driver.findElement(By.cssSelector("span.dd-pointer.dd-pointer-down")).click();
+        driver.findElement(By.xpath("//div[@id='testRunSelect']/ul/li[2]/a/label")).click();
+        driver.findElement(By.linkText("choose assigned user")).click();
+        driver.findElement(By.xpath("//div[@id='assignedTestRun']/ul/li[2]/a/label")).click();
+        driver.findElement(By.xpath("//div[@id='assignedTestRun']/ul/li[2]/a/label")).submit();
         WebElement errorMessage = driver.findElement(By.className("message"));
         assertTrue(errorMessage.getText().contains("can't be blank"));
     }
 }
+
+// Create instance of Javascript executor
+// JavascriptExecutor je = (JavascriptExecutor) driver;
+//Identify the WebElement which will appear after scrolling down
+// WebElement element = driver.findElement(By.xpath("//div[@id='mCSB_4_dragger_vertical']"));
+// now execute query which actually will scroll until that element is not appeared on page.
+// je.executeScript("arguments[0].scrollIntoView(true);",element);
+// Thread.sleep(4000);
+//driver.findElement(By.xpath("//div[@id='mCSB_4_container']/div/div[6]/input")).click();
+// Thread.sleep(4000);
